3afe6f593fbcb8b3f91b143bffb248ad6361c6f4
ZMT <zmt_email@163.com>
2023-08-19 12:37:21 +0800

    chore: config

--- vue.config.js
+++ vue.config.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-const path = require('path')
-const webpack = require('webpack')
-function resolve (dir) {
-  return path.join(__dirname, dir)
-}
-
-const name = 'vue Element Admin' // page title
-
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following method:
-// port = 9527 npm run dev OR npm run dev --port = 9527
-// eslint-disable-next-line no-unused-vars
-const port = process.env.port || process.env.npm_config_port || 3000 // dev port
-
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
-module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: './',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
-  devServer: {
-    open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    proxy: {
-    // change xxx-api/login => mock/login
-    // detail: https://cli.vuejs.org/config/#devserver-proxy
-      '/api': {
-      target: 'http://192.168.2.240:8080',
-      //   target: 'http://47.114.6.168:8080/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  },
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: name,
-    resolve: {
-      alias: {
-        '@': resolve('src')
-      }
-    }
-  }
-}
-=======
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 function resolve(dir) {
@@ -81,4 +18,3 @@ module.exports = defineConfig({
     },
   },
 });
->>>>>>> 6d726aca7b36707575d3d47ca165a0a551942d2e
